59e1e874dd82dd78fda543cd79a8ba3085737762
Anže Stražar <strazar@gmail.com>
2020-07-06 11:58:34 +0200

    Code refactoring

--- main.js
+++ main.js
@@ -1,26 +1,33 @@
-var express = require('express')
-const scraper = require('./scraper')
+const express = require('express');
+const scraper = require('./scraper');
 
-var app = express()
+var app = express();
 
 app.get('/stories', async function (req, res) {
-    result = await scraper.parseLatestCnetArticles()
-    // result = await scraper.parseArticle('https://www.cnet.com/news/best-meat-delivery-deals-for-july-4th/')
-    res.send(result)
+    result = await scraper.parseLatestCnetArticles();
+    res.json(result);
 })
 
 app.get('/pdf', async function (req, res) {
-    result = await scraper.printPDFArticle(decodeURIComponent(req.query.url))
-    res.setHeader('Content-disposition', 'inline; filename="' + new Date().getTime() + '"');
+    if (!req.query.url) {
+        res.status(400).json({ error: 'Must provide URL parameter!' });
+        return;
+    }
+    result = await scraper.printPDFArticle(decodeURIComponent(req.query.url));
+    res.setHeader('Content-disposition', 'inline; filename="' + new Date().getTime() + '.pdf"');
     res.setHeader('Content-type', 'application/pdf');
-    res.send(result)
+    res.send(result);
 })
 
 app.get('/screenshot', async function (req, res) {
-    result = await scraper.screenshotArticle(decodeURIComponent(req.query.url))
-    res.setHeader('Content-disposition', 'inline; filename="' + new Date().getTime() + '"');
+    if (!req.query.url) {
+        res.status(400).json({ error: 'Must provide URL parameter!' });
+        return;
+    }
+    result = await scraper.screenshotArticle(decodeURIComponent(req.query.url));
+    res.setHeader('Content-disposition', 'inline; filename="' + new Date().getTime() + '.jpg"');
     res.setHeader('Content-type', 'image/jpeg');
-    res.send(result)
+    res.send(result);
 })
 
-app.listen(3000)
+app.listen(3000);
--- scraper.js
+++ scraper.js
@@ -1,117 +1,110 @@
-const puppeteer = require('puppeteer-core');
-const config = require('./config.json')
+const puppeteer = require('puppeteer');
+const config = require('./config.json');
 
+/* fffgf
+ * jgngng
+ *
+*/
 async function parseLatestCnetArticles() {
-    const browser = await puppeteer.launch({
-        headless: false, // Remove, if you want to have headless mode; by default puppeteer launch method have headless option true
-        executablePath: '/usr/bin/google-chrome'
-    });
+    const browser = await puppeteer.launch({});
     const page = await browser.newPage();
-
-    // const link = 'https://www.google.com'; This way is maybe better? First declare link variable
-    // await page.goto(link);
 
     await page.goto('https://www.cnet.com/');
 
-    /** waitfor while loding the page, otherwise evaulate method will get failed. */
     const topStories = await page.evaluate(async () => {
         try {
-            let data = []
+            const data = [];
             const list = document.querySelectorAll('.col-5 .col-4');
             for (i = 0; i < 5; i++) {
-                let link = list[i].querySelector('a').href
-                newsHeader = list[i].querySelector('a').innerText
-                shortSummary = list[i].querySelector('p').innerText.trim()
-                data.push({ link, newsHeader, shortSummary })
+                const link = list[i].querySelector('a').href;
+                const newsHeader = list[i].querySelector('a').innerText;
+                const shortSummary = list[i].querySelector('p').innerText.trim();
+                data.push({ link, newsHeader, shortSummary });
             }
             return data;
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     })
 
-    let results = []
+    const results = [];
     for (const story of topStories) {
-        let result = await parseArticle(story.link, browser)
-        result.newsHeader = story.newsHeader
-        result.shortSummary = story.shortSummary
-        results.push(result)
+        const result = await parseArticle(story.link, browser);
+        result.newsHeader = story.newsHeader;
+        result.shortSummary = story.shortSummary;
+        results.push(result);
     }
     await browser.close();
-    // console.log(results)
     return results
 
 }
 
 async function parseArticle(link, browser) {
     try {
-
-        let shouldCloseBrowser = false
+        let shouldCloseBrowser = false;
 
         if (!browser) {
-            shouldCloseBrowser = true
-            browser = await puppeteer.launch({
-                headless: false, // Remove, if you want to have headless mode; by default puppeteer launch method have headless option true
-                executablePath: '/usr/bin/google-chrome'
-            });
+            shouldCloseBrowser = true;
+            browser = await puppeteer.launch({});
         }
 
-        const page = await browser.newPage()
+        const page = await browser.newPage();
 
-        await page.goto(link)
-        const url = new URL(link)
-        var category = url.pathname.split('/')[1]
+        await page.goto(link);
+        const url = new URL(link);
+        const category = url.pathname.split('/')[1];
 
         const result = await page.evaluate(() => {
-            let newsHeader = null
-            let newsHeaderElement = document.querySelector('.speakableText')
+            let newsHeader = null;
+            let newsHeaderElement = document.querySelector('.speakableText');
             if (newsHeaderElement) {
-                newsHeader = newsHeaderElement.innerText
+                newsHeader = newsHeaderElement.innerText;
             }
 
-            let shortSummary = null
-            let shortSummaryElement = document.querySelector('.c-head_dek')
+            let shortSummary = null;
+            let shortSummaryElement = document.querySelector('.c-head_dek');
             if (shortSummaryElement) {
-                shortSummary = shortSummaryElement.innerText
+                shortSummary = shortSummaryElement.innerText;
             }
 
-            let tagElements = document.querySelector('#article-body .tagList')
-            let tags = []
-
+            const tags = [];
+            let tagElements = document.querySelector('#article-body .tagList');
+            // If the article is in video category, then timestamp is associated with different class as in other categories
+            if (!tagElements) {
+                tagElements = document.querySelector('#videoPage .videoTags');
+            }
             if (tagElements) {
-                tagElements = tagElements.children
+                tagElements = tagElements.children;
 
                 for (const a of tagElements) {
                     if (a.innerText) {
-                        tags.push(a.innerText)
+                        tags.push(a.innerText);
                     }
-                };
-            }
-
-            let authors = []
-            let authorElements = document.querySelectorAll('.author')
-            if (authorElements) {
-                // author = authorElements.innerText
-                for (const a of authorElements) {
-                    authors.push(a.innerText)
                 }
             }
 
-            let publishTimeStamp = null
-            let publishTimeStampElement = document.querySelector('.c-assetAuthor_date time')
-            // If the article is in video category, then timestamp is associated with different class as in other
+            const authors = [];
+            let authorElements = document.querySelectorAll('.author');
+            if (authorElements) {
+                for (const a of authorElements) {
+                    authors.push(a.innerText);
+                }
+            }
+
+            let publishTimeStamp = null;
+            let publishTimeStampElement = document.querySelector('.c-assetAuthor_date time');
+            // If the article is in video category, then timestamp is associated with different class as in other categories
             if (!publishTimeStampElement) {
-                publishTimeStampElement = document.querySelector('.info-timeDate time')
+                publishTimeStampElement = document.querySelector('.info-timeDate time');
             }
             if (publishTimeStampElement) {
-                publishTimeStamp = publishTimeStampElement.getAttribute('datetime')
+                publishTimeStamp = publishTimeStampElement.getAttribute('datetime');
             }
 
-
-            let image = null
-            let imageElement = document.querySelector('.imageContainer span img')
+            let image = null;
+            let imageElement = document.querySelector('.imageContainer span img');
             if (imageElement) {
-                image = imageElement.getAttribute('src')
+                image = imageElement.getAttribute('src');
             }
 
             return {
@@ -122,80 +115,72 @@ async function parseArticle(link, browser) {
                 publishTimeStamp,
                 image
             }
-        })
+        });
 
-        result.category = category
-        result.link = link
-        result.pdfLink = config.frontURL + '/pdf?url=' + encodeURIComponent(link)
-        result.screenshotLink = config.frontURL + '/screenshot?url=' + encodeURIComponent(link)
+        result.category = category;
+        result.link = link;
+        result.pdfLink = config.frontURL + '/pdf?url=' + encodeURIComponent(link);
+        result.screenshotLink = config.frontURL + '/screenshot?url=' + encodeURIComponent(link);
 
         if (shouldCloseBrowser) {
             await browser.close();
         }
 
-        return result
+        return result;
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
 async function printPDFArticle(link, browser) {
     try {
-
-        let shouldCloseBrowser = false
+        let shouldCloseBrowser = false;
 
         if (!browser) {
-            shouldCloseBrowser = true
-            browser = await puppeteer.launch({
-                headless: true, // Remove, if you want to have headless mode; by default puppeteer launch method have headless option true
-                executablePath: '/usr/bin/google-chrome'
-            });
+            shouldCloseBrowser = true;
+            browser = await puppeteer.launch({});
         }
 
-        const page = await browser.newPage()
+        const page = await browser.newPage();
 
-        await page.goto(link)
-        const articlePDF = await page.pdf({ format: 'A4', printBackground: true })
+        await page.goto(link);
+        const articlePDF = await page.pdf({ format: 'A4', printBackground: true });
 
         if (shouldCloseBrowser) {
             await browser.close();
         }
 
-        return articlePDF
+        return articlePDF;
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
 async function screenshotArticle(link, browser) {
     try {
-
-        let shouldCloseBrowser = false
+        let shouldCloseBrowser = false;
 
         if (!browser) {
-            shouldCloseBrowser = true
-            browser = await puppeteer.launch({
-                headless: true, // Remove, if you want to have headless mode; by default puppeteer launch method have headless option true
-                executablePath: '/usr/bin/google-chrome'
-            });
+            shouldCloseBrowser = true;
+            browser = await puppeteer.launch({});
         }
 
-        const page = await browser.newPage()
+        const page = await browser.newPage();
 
-        await page.goto(link)
-        const articleScreenshot = await page.screenshot({ fullPage: true })
+        await page.goto(link);
+        const articleScreenshot = await page.screenshot({ fullPage: true });
 
         if (shouldCloseBrowser) {
             await browser.close();
         }
 
-        return articleScreenshot
+        return articleScreenshot;
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
-exports.parseLatestCnetArticles = parseLatestCnetArticles
-exports.parseArticle = parseArticle
-exports.printPDFArticle = printPDFArticle
-exports.screenshotArticle = screenshotArticle
+exports.parseLatestCnetArticles = parseLatestCnetArticles;
+exports.parseArticle = parseArticle;
+exports.printPDFArticle = printPDFArticle;
+exports.screenshotArticle = screenshotArticle;
